5c2c1ec5666dcbb7646f9cc92004ce971ea65bf6
Dominika Macura <dmdoris177@gmail.com>
2022-06-22 13:35:24 +0200

    fix: fix ts typos

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,59 +1,69 @@
 import React, { createContext, useContext, useState } from "react";
 
 type TasksContextValues = {
-  sections: SectionDetails[];
-  tasks: TaskDetails[];
-  setTasks(tasks: TaskDetails[]): void;
+  data: DataDetails[];
+  setData(tasks: DataDetails[]): void;
 };
 
 const TasksContext = createContext<TasksContextValues>({
-  sections: [],
-  tasks: [],
-  setTasks() {},
+  data: [],
+  setData() {},
 });
 
 export function useTasksContext() {
   return useContext(TasksContext);
 }
 
-export type SectionDetails = {
-  title: string;
-  id: number;
-};
-
 export type TaskDetails = {
   task: string;
   id: number;
 };
 
+export type DataDetails = {
+  name: string;
+  items: TaskDetails[];
+};
+
 export function TasksProvider({ children }: any) {
-  const [sections, setSections] = useState<SectionDetails[]>([
+  const [data, setData] = useState<DataDetails[]>([
     {
-      title: "To do",
-      id: 1,
+      name: "To do",
+      items: [
+        {
+          task: "Prepare for a party",
+          id: 1,
+        },
+        {
+          task: "Go for a walk",
+          id: 2,
+        },
+      ],
     },
     {
-      title: "In progress",
-      id: 2,
+      name: "In progress",
+      items: [
+        {
+          task: "Finish the book",
+          id: 3,
+        },
+      ],
     },
     {
-      title: "Completed",
-      id: 3,
+      name: "Completed",
+      items: [
+        {
+          task: "Practice yoga",
+          id: 4,
+        },
+        {
+          task: "Do shopping for a barbecue",
+          id: 5,
+        },
+      ],
     },
   ]);
 
-  const [tasks, setTasks] = useState<TaskDetails[]>([
-    {
-      task: "Prepare dinner",
-      id: 1,
-    },
-    {
-      task: "Go for a walk",
-      id: 2,
-    },
-  ]);
-
-  const value = { sections, tasks, setTasks };
+  const value = { data, setData };
 
   return <TasksContext.Provider value={value}>{children}</TasksContext.Provider>;
 }
